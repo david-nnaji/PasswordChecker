27607482e51078842357de55a9f53010bcba50ae
David Nnaji <nnaji@cua.edu>
2025-10-11 13:34:49 -0400

    testing again

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log("Hello, World!");
+console.log("This is a sample JavaScript file.");
+console.log("It contains multiple lines of code.");
+console.log("Each line performs a simple task.");
+console.log("This is the last line of the file.");  
